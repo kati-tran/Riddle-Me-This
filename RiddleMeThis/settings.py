24d3fe61d96303f55a7d05a6aca519b2ebfcb7f0
whydude <sandra909@live.com>
2019-07-20 17:52:42 -0700

    started up
    - finished set up
    - added main html file
    - removed unnecessary packages from requirements.txt
    - added gitignore
    - removed pycache files

--- RiddleMeThis/settings.py
+++ RiddleMeThis/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',
 ]
 
 MIDDLEWARE = [
@@ -119,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(os.path.dirname(__file__), 'static').replace('\\','/'),
+)
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
