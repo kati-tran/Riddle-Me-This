a6e11846bd2be0ed51c8c8aa9e8ac58a36002e10
alexkumar520 <43835304+alexkumar520@users.noreply.github.com>
2019-07-25 14:49:14 -0700

    test changes

--- chat/views.py
+++ chat/views.py
@@ -1,12 +1,10 @@
+# chat/views.py
 from django.shortcuts import render
 from django.utils.safestring import mark_safe
 import json
 
-# Create your views here.
-
 def index(request):
     return render(request, 'chat/index.html', {})
-
 
 def room(request, room_name):
     return render(request, 'chat/room.html', {
