778b0391dfc0aaa975a34a856215285ec21245e3
alexkumar520 <43835304+alexkumar520@users.noreply.github.com>
2019-07-26 11:16:53 -0700

    gave up on life real quick

--- chat/asgi.py
+++ chat/asgi.py
@@ -1,7 +1,5 @@
 import os
-import django
-from channels.routing import get_default_application
+import channels.asgi
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chat.settings")
-django.setup()
-application = get_default_application()
+channel_layer = channels.asgi.get_channel_layer()
