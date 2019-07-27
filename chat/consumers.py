680e0344a4175f97a1ba5fa0e3d41937654bf20c
alexkumar520 <43835304+alexkumar520@users.noreply.github.com>
2019-07-26 18:17:43 -0700

    testing some bs

--- chat/consumers.py
+++ chat/consumers.py
@@ -2,11 +2,13 @@
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 import json
+from math import randint
 
 
 class ChatConsumer(WebsocketConsumer):
 
     def connect(self):
+        print(randint(1, 100))
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
 
