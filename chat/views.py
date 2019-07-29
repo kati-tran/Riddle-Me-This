6811b9777ced00b7fc32d275214be62a33918120
alexkumar520 <43835304+alexkumar520@users.noreply.github.com>
2019-07-28 21:05:57 -0700

    Revert "if this doesnt work ima fkin punch the wall"
    This reverts commit e15ae357a50054b603a310518ae18c0265f6c7f2.

--- chat/views.py
+++ chat/views.py
@@ -7,8 +7,6 @@ def index(request):
     return render(request, 'chat/enter_chat.html', {})
 
 def room(request, room_name):
-	username = request.GET["username"]
-	final = str(username)
     return render(request, 'chat/room.html', {
-        'room_name_json': mark_safe(json.dumps(room_name)), 'username': final
+        'room_name_json': mark_safe(json.dumps(room_name)), 'username': str(request.GET["username"])
     })
