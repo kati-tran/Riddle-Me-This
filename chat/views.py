ce378d97c2c50ed7c803b16df18aea116730f731
jigglequack <45055772+jigglequack@users.noreply.github.com>
2019-08-01 01:41:03 -0700

    testing riddle print

--- chat/views.py
+++ chat/views.py
@@ -1,12 +1,14 @@
 # chat/views.py
 from django.shortcuts import render
 from django.utils.safestring import mark_safe
+from django.http import HttpResponse
 import json
+import generateRiddles
 
 def index(request):
     return render(request, 'chat/index.html', {})
 
 def room(request, room_name):
     return render(request, 'chat/room.html', {
-        'room_name_json': mark_safe(json.dumps(room_name)), 'username': request.session['username']
+        'room_name_json': mark_safe(json.dumps(room_name)), 'username': request.session['username'], 'riddles': HttpResponse( json.dumps(generateRiddles.getRiddle())) 
     })
