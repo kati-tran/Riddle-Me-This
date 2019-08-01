331a6c4595fca338cf64de45b7a19ae2975f96ac
jigglequack <45055772+jigglequack@users.noreply.github.com>
2019-08-01 02:22:09 -0700

    show riddle?
    imported generateRiddles

--- chat/views.py
+++ chat/views.py
@@ -3,12 +3,12 @@ from django.shortcuts import render
 from django.utils.safestring import mark_safe
 from django.http import HttpResponse
 import json
-import generateRiddles
+from chat.generateRiddles import *
 
 def index(request):
     return render(request, 'chat/index.html', {})
 
 def room(request, room_name):
     return render(request, 'chat/room.html', {
-        'room_name_json': mark_safe(json.dumps(room_name)), 'username': request.session['username'], 'riddles': HttpResponse( json.dumps(generateRiddles.getRiddle())) 
+        'room_name_json': mark_safe(json.dumps(room_name)), 'username': request.session['username'], 'riddles': HttpResponse( json.dumps(getRiddle())) 
     })
--- django_channels_heroku/settings.py
+++ django_channels_heroku/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'chat',
     'channels',
     'app',
-    'generateRiddles',
 ]
 
 MIDDLEWARE = [
