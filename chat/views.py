dfe53faa4a950274b4901c0cd32076652df1e0cf
whydude <sandra909@live.com>
2019-08-01 11:17:42 -0700

    no bs4 error

--- chat/views.py
+++ chat/views.py
@@ -3,14 +3,15 @@ from django.shortcuts import render
 from django.utils.safestring import mark_safe
 #from django.http import HttpResponse
 import json
-#from chat.generateRiddles import *
+from chat import generateRiddles as gr
 
 def index(request):
     return render(request, 'chat/index.html', {})
 
 def room(request, room_name):
     return render(request, 'chat/room.html', {
-        'room_name_json': mark_safe(json.dumps(room_name)), 'username': request.session['username'] 
+        'room_name_json': mark_safe(json.dumps(room_name)), 'username': request.session['username'],
+        'riddles': json.dumps(gr.getRiddle()),
     })
 
 #'riddles': HttpResponse( json.dumps(getRiddle())
