e58198979ff49eea2d781533ce5738422bef207f
jigglequack <45055772+jigglequack@users.noreply.github.com>
2019-08-01 02:58:57 -0700

    oops sorry didnt remove more things

--- chat/views.py
+++ chat/views.py
@@ -1,7 +1,7 @@
 # chat/views.py
 from django.shortcuts import render
 from django.utils.safestring import mark_safe
-from django.http import HttpResponse
+#from django.http import HttpResponse
 import json
 #from chat.generateRiddles import *
 
@@ -10,5 +10,7 @@ def index(request):
 
 def room(request, room_name):
     return render(request, 'chat/room.html', {
-        'room_name_json': mark_safe(json.dumps(room_name)), 'username': request.session['username'], 'riddles': HttpResponse( json.dumps(getRiddle())) 
+        'room_name_json': mark_safe(json.dumps(room_name)), 'username': request.session['username'] 
     })
+
+#'riddles': HttpResponse( json.dumps(getRiddle())
