5b3a32184ff3c1e33a677b411a1fc1002f7eae59
alexkumar520 <43835304+alexkumar520@users.noreply.github.com>
2019-08-02 13:55:51 -0700

    Revert "test to get user response, might break website"
    This reverts commit 4140bd230ea9afb5213f9c50bafb6386a3d312ee.

--- chat/forms.py
+++
@@ -1,4 +0,0 @@
-from django import forms
-
-class UserIn(forms.Form):
-    response = forms.CharField(max_length=100)
--- chat/views.py
+++ chat/views.py
@@ -1,7 +1,7 @@
 # chat/views.py
 from django.shortcuts import render
 from django.utils.safestring import mark_safe
-from .forms import UserIn
+#from django.http import HttpResponse
 import json
 from chat import generateRiddles as gr
 
@@ -9,8 +9,6 @@ def index(request):
     return render(request, 'chat/index.html', {})
 
 def room(request, room_name):
-	if form.is_valid():
-		print(form.cleaned_data['response'])
     return render(request, 'chat/room.html', {
         'room_name_json': mark_safe(json.dumps(room_name)), 'username': request.session['username'],
         'riddles': json.dumps(list((gr.allRiddles(dict(),3)).items())),
