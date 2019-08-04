2758ab5d12a70000b20281a15f67fcc79d02b7ba
alexkumar520 <43835304+alexkumar520@users.noreply.github.com>
2019-08-03 21:06:48 -0700

    retest because of method url error?

--- app.py
+++ app.py
@@ -1,21 +1,18 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO
+from flask.ext.socketio import SocketIO
+import json
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
 socketio = SocketIO(app)
 
-@app.route('/')
-def sessions():
-    return render_template('session.html')
+@app.route("/")
+def index():
+    return render_template('index.html',)
 
-def messageReceived(methods=['GET', 'POST']):
-    print('message was received!!!')
+@socketio.on('send_message')
+def handle_source(json_data):
+    text = json_data['message'].encode('ascii', 'ignore')
+    socketio.emit('echo', {'echo': 'Server Says: '+text})
 
-@socketio.on('my event')
-def handle_my_custom_event(json, methods=['GET', 'POST']):
-    print('received my event: ' + str(json))
-    socketio.emit('my response', json, callback=messageReceived)
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     socketio.run(app, debug=True)
