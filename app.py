d4072d23e5882a3b879b3de9169bb4358f4322b5
whydude <sandra909@live.com>
2019-08-04 01:03:17 -0700

    re-add html
    - re-added html for flask
    - added gitignore
    - updated req txt

--- app.py
+++ app.py
@@ -1,11 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session
 from flask_socketio import SocketIO
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
+app.secret_key = 'why'
 socketio = SocketIO(app)
 
-@app.route('/')
+
+@app.route('/', methods = ['GET','POST'])
+def main():
+    if request.method == 'GET':
+        return render_template('index.html')
+    elif request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('lobby', username = session['username']))
+    
+@app.route('/lobby', methods = ['GET'])
+def lobby():
+    if request.method == 'GET':
+        return render_template('lobby.html', username = session['username'])
+
+@app.route('/contact')
+def contact():
+    return render_template('index.html')
+
+@app.route('/terms_conditions')
+def tos():
+    return render_template('tos.html')
+
+@app.route('/game')
 def sessions():
     return render_template('session.html')
 
